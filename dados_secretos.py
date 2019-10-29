89f487669665e4bb0d20c35106ddf554071cba28
Luis Fernando Boschiero Kumruyan <nando@kyan.com.br>
2019-10-29 10:21:50 -0300

    Add files via upload

---
+++ dados_secretos.py
@@ -0,0 +1,5 @@
+db_user = "vqntlmgadwynwp"
+db_pswd = "4ba9d7a162791ede90a0a267292b6922fdc9ec7aa321a4ff6396d64b736e0603"
+db_host = "ec2-54-235-163-246.compute-1.amazonaws.com"
+db_port = "5432"
+db_db = "da2fg0dip1jn7u"
